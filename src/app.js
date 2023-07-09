5d307d58350a3632d45e67542715990fc33579d2
Oleksandr Chervonniy <sheph@ukr.net>
2023-07-09 16:08:19 +0200

    added view pages and routing, added react render

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import { MainView } from './views/main/main.js';
+
+class App {
+	routes = [{ path: '', view: MainView }];
+	appState = {
+		favorites: [],
+	};
+
+	constructor() {
+		window.addEventListener('hashchange', this.route.bind(this));
+		this.route();
+	}
+
+	route() {
+		if (this.currentView) {
+			this.currentView.destroy();
+		}
+		const view = this.routes.find((r) => r.path == location.hash).view;
+		this.currentView = new view(this.appState);
+		this.currentView.render();
+	}
+}
+
+new App();
